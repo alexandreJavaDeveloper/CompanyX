8d76c4b5ca5007bd7cd1e190eda8cc9ca2b4342a
Alexandre <alexandreply@hotmail.com>
2018-09-15 11:58:21 +0100

    Was GET just for testing. Now is POST the method.

--- src/main/java/com/companyx/rest/MoneyTransferService.java
+++ src/main/java/com/companyx/rest/MoneyTransferService.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -48,7 +48,7 @@ public class MoneyTransferService {
 	 * @param moneyToTransfer
 	 * @return response as JSON result
 	 */
-	@GET
+	@POST
 	@Produces(MoneyTransferService.MEDIA_TYPE)
 	@Path("/transfer/{receiverAccountNumber}/{senderAccountNumber}/{moneyToTransfer}")
 	public javax.ws.rs.core.Response transferMoneyService(
